3d7a9e87dd0dffaa5c2fa994d38f80835af511e1
LeoArouca <leo.arouca@purecontrolpilates.com>
2017-08-19 10:47:57 +0800

    Finish search form

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
+// controlled field is a field that the value is set by the state
 export default class SearchBar extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = { term: ''};
+
+    // To bind this to the component so in the method this is the component not the field
+    // Was thrwoing error b4
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event){
+    this.setState({term: event.target.value});
+  }
+
+  onFormSubmit(event){
+    // Stops submit auto call from the form
+    event.preventDefault();
+
+    // Need to go fetch the data
+  }
+
   render(){
     return(
-      <form className='input-group'>
-        <input />
+      <form onSubmit={this.onFormSubmit} className='input-group'>
+        <input
+          placeholder='Get a five day forcast in your favorite cities'
+          className='form-control'
+          value={this.state.term}
+          onChange={this.onInputChange}/>
         <span className='input-group-btn'>
           <button type='submit' className='btn btn-secondary'>Submit</button>
         </span>
